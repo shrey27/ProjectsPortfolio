2f3f339a9b4bb0e0bf8bb75fd65f48e9b86e0d97
Shrey Pandey <shreypandey27@gmail.com>
2022-05-15 22:39:19 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,53 @@ export function Footer() {
   return (
     <footer class='section-5 center'>
       <div class='social-media'>
-        <a href='#none' class='social-media-link'>
-          <i class='fab fa-github-square'></i>
+        <a
+          href='https://linkedin.com/in/shrey27'
+          class='social-media-link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img
+            src='https://img.icons8.com/color/28/000000/linkedin.png'
+            alt='link'
+          />
         </a>
-        <a href='#none' class='social-media-link'>
-          <i class='fab fa-youtube'></i>
+        <a
+          href='https://twitter.com/shrey271'
+          class='social-media-link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img
+            src='https://img.icons8.com/color/28/000000/twitter--v1.png'
+            alt='link'
+          />
         </a>
-        <a href='#none' class='social-media-link'>
-          <i class='fab fa-facebook-square'></i>
+        <a
+          href='https://github.com/shrey27'
+          class='social-media-link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img
+            src='https://img.icons8.com/material-outlined/28/000000/github.png'
+            alt='link'
+          />
         </a>
-        <a href='#none' class='social-media-link'>
-          <i class='fab fa-instagram-square'></i>
+        <a
+          href='https://shrey027.hashnode.dev/'
+          class='social-media-link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img
+            src='https://img.icons8.com/color/28/000000/hashnode.png'
+            alt='link'
+          />
         </a>
       </div>
       <p class='copyright'>
-        Copyright &copy; CodeAndCreate. All Rights Reserved
+        Copyright &copy; Shrey Pandey. All Rights Reserved
       </p>
     </footer>
   );
